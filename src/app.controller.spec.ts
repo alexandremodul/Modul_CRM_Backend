72e422e961deb413508efed4ef8fc115f71dd443
AlexandreMaffeis <alexandre@modul.com.br>
2024-08-21 15:09:47 -0300

    Teste de Github Actions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Modul CRM Backend"', () => {
+      expect(appController.getHello()).toBe('Modul CRM Backend');
     });
   });
 });
