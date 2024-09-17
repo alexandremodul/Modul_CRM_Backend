b023f295fc87074c49cf9c66314a1a36c4a867be
AlexandreMaffeis <alexandre@modul.com.br>
2024-09-17 12:00:55 -0300

    Teste depois de Criado o .env no servidor se não vai sobrescrever o arquivo

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         console.log('Database config:', {
           host: process.env.DB_HOST,
           username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
           database: process.env.DB_DATABASE,
           port: process.env.DB_PORT,
         });
